a658c218fb19cf93dd9b8954f537c3c446e79897
Giovanni Squillero <giovanni.squillero@polito.it>
2017-05-11 15:21:15 +0200

    Clean up

--- src/MakeDocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Generating doc files"
-find Libs -name "*.h" -print -exec docify {} {}.doc++ \;
-find Frontends -name "*.h" -print -exec docify {} {}.doc++ \;
-doc++ --all --filenames  --full-toc --dir Docs/Html `find . -name "*.doc++" -print`
-find . -name "*.doc++" -exec rm {} \;
--- src/UpdateTeam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILE=$1
-TMP=tmpfile.$$
-
-echo Updating $FILE
-
-./UpdateTeam.pl <$FILE >$TMP
-mv $TMP $FILE
